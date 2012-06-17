d7771e550ee02b7725d6ab64919db71b1c392bbf
yokoo <ferrylikeboy@gmail.com>
2012-06-17 17:29:33 +0900

    first commit

---
+++ app/models/note.rb
@@ -0,0 +1,3 @@
+class Note < ActiveRecord::Base
+  attr_accessible :memo, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yokoo1::Application.config.secret_token = 'c45d23eebe5ca1d2bd795cd91c5c5a5e0c7734beac12730cb0e04c08a255245580016354508cfefa53d9c2c574aa5441025f58d0245ab15ef52f41c5117d893d'
